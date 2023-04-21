97128be8a9236a402251fe5aec0ccb44f2baf314
Denis Klochkov <den_klochkov@mail.ru>
2023-04-22 02:39:41 +0300

    final commit

--- vk_course_work.py
+++ vk_course_work.py
@@ -2,6 +2,7 @@ from pprint import pprint
 import json
 import requests
 import configparser
+from tqdm import tqdm
 
 class VKUser:
     def __init__(self):
@@ -9,7 +10,7 @@ class VKUser:
         self.vk_user_id = input('Введите свой ID пользователя VK или короткое имя (screen_name): ')
         self.id_album = input("Введите ID альбома: ")
         self.count = input('Введите количество фотографий: ')
-        self.dict_with_photo = {}
+        self.photo_dict = {}
 
     def get_photo(self):
         URL = 'https://api.vk.com/method/users.get'
@@ -28,20 +29,21 @@ class VKUser:
         res_get_photo = requests.get(URL, params=params).json()
         for file in res_get_photo['response']['items']:
             file_url = max(file['sizes'], key=lambda x: vk_sizes[x['type']])
-            if file['likes']['count'] in self.dict_with_photo.keys():
-                self.dict_with_photo[f"{file['likes']['count']}_{file['date']}"] =  file_url['url']
+            names = file['likes']['count']
+            if names in self.photo_dict.keys():
+                self.photo_dict[f"{names}_{file['date']}"] =  file_url['url']
             else:
-                self.dict_with_photo[file['likes']['count']] =  file_url['url']
+                self.photo_dict[names] =  file_url['url']
         return
     
     def get_json(self, file):
         with open('result_json.json', 'w') as f:
-            json.dump(file,f, ensure_ascii=False, indent=2)
+            json.dump(file, f, ensure_ascii=False, indent=2)
          
 class YandexDisk():
     def __init__(self, poligon):
         self.poligon = poligon
-    
+
     def get_headers(self):
         
         return {
@@ -49,30 +51,46 @@ class YandexDisk():
             'Authorization': 'OAuth {}'.format(self.poligon)
         }           
     
-
-    def upload_file_to_disk(self, dict_with_photo, folder_name):
+    def create_get_folder(self, photo_dict):
+        folder_name = input(f'Введите название папки Яндекс Диска для загрузки фото: ')
+        url_disk = 'https://cloud-api.yandex.net/v1/disk/resources'
+        headers = {'Content-Type': 'application/json', 'Authorization': f'OAuth {self.poligon}'}
+        params_folder = {'path': folder_name}
+        response_folder = requests.put(url_disk, params=params_folder, headers=headers)
+        if response_folder.status_code == 409:
+            flag = input('Папка с указанным названием уже существует. Желаете создать новую папку с другим именем? Введите да или нет: ')
+            if flag.lower() == 'да'.lower():
+                self.create_get_folder(photo_dict)
+                return
+            if flag.lower() == 'нет'.lower():
+                print(f'Фотографии будут загружены в папку "{folder_name}".')
+            else:
+                print(f'Вы ввели некорректную команду. Работа программы будет прервана. Вернитесь к выбору названия папки.')    
+                self.create_get_folder(photo_dict)
+                return
         upload_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
         headers = self.get_headers()
-        for name, size in dict_with_photo.items():
-            params = {'path': f'{folder_name}/{name}.jpg','url': size}
-            response = requests.post(upload_url, headers=headers, params=params)   
-        if response.status_code == 202:
-            print('Фотографии загружены')
+        for name, size in tqdm(photo_dict.items(), desc='Processing photos', unit='photo'):            
+            params_upload = {'path': f'{folder_name}/{name}.jpg','url': size}
+            response_upload = requests.post(upload_url, headers=headers, params=params_upload)   
+        if response_upload.status_code == 202:
+            print(f'Фотографии загружены в папку "{folder_name}".')
         else:
-            print('Произошла ошибка')    
-        
-
+            print (f'Произошла ошибка.')    
+            return   
+            
 if __name__ == '__main__':
     config = configparser.ConfigParser()
     config.read('settings.ini')
     config.sections()
     poligon = config['DEFAULT']['POLIGON_YA']
-    folder_name = 'VK'
     vk = VKUser()
     vk.get_photo()
-    vk.get_json(vk.dict_with_photo)
+    vk.get_json(vk.photo_dict)
     ya = YandexDisk(poligon=poligon)
-    ya.upload_file_to_disk(vk.dict_with_photo, folder_name)
+    ya.create_get_folder(vk.photo_dict)
+   
+    
 
 
     
